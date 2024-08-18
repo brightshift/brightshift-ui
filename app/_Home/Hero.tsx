446f6004a239df473298aa4e3a06740e6d6ac693
Chipsii <hasibfire24@gmail.com>
2024-08-19 00:43:47 +0600

    fix: fixed the gif overlapping the nav

--- app/_Home/Hero.tsx
+++ app/_Home/Hero.tsx
@@ -47,7 +47,7 @@ export const Hero = ({ ...props }: Props) => {
       </div>
       <div className="relative z-20 flex items-center justify-center py-10">
         <Image
-        className="rounded-lg border-2 border-black dark:border-white shadow-2xl shadow-background"
+          className="rounded-lg border-2 border-black shadow-2xl shadow-background dark:border-white"
           src="https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExdzA3dnRoM2I1eDZqMjd5ZG5wZ3BtbGIyZWF6ZDdjbGQ1dDN2b2lhZCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/l2JdTkHW1KZPdvdS0/giphy.gif"
           width={1024}
           height={500}
