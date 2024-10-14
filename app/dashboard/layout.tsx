af58d2c8d4f736e96b909025dced41120016a3a2
yeasin2002 <mdkawsarislam2002@gmail.com>
2024-10-14 21:09:13 +0600

    refactor: removed legacy sidebar

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,19 +1,16 @@
 "use client"
 
 import React, { PropsWithChildren } from "react"
-import { dashboardSidebarData } from "@/data"
 
 import { DashboardProvider } from "./dashboardProvider"
 import { DashboardNav } from "./main-components"
 import { DashboardDragSidebar } from "./main-components/DashboardDragSidebar"
-import DashboardSizePanel from "./main-components/DashboardSidePanel"
 
 const DashboardLayout = ({ children }: PropsWithChildren) => {
   return (
     <DashboardProvider>
       <DashboardNav />
       <div className="flex">
-        {/* <DashboardSizePanel links={dashboardSidebarData} /> */}
         <DashboardDragSidebar />
 
         {children}
